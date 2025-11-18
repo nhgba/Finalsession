e9e3a8acec7098fdbdee27b03b9b824458691598
bao911 <nhgbaoa24010@cusc.ctu.edu.vn>
2025-11-18 15:06:50 +0700

    first commit

--- config/database.config.js
+++ config/database.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://nhgbao:bao911@cluster0.t0ayg0h.mongodb.net/?appName=Cluster0');
+        await mongoose.connect('mongodb+srv://nhgbao:bao123@cluster0.t0ayg0h.mongodb.net/?appName=Cluster0');
         console.log('MongoDB connected successfully!');
     } catch (err) {
         console.error('MongoDB connection error:', err);
